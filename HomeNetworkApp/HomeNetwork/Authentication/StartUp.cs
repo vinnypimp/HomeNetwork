22c92e23100da78a2c6c28e384202b41eeadf754
vinnypimp <vpimpinella@gmail.com>
2016-03-24 23:24:21 -0400

    Added Auth Library

---
+++ HomeNetworkApp/HomeNetwork/Authentication/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeNetwork.View;
+using HomeNetwork.ViewModel;
+
+namespace Authentication
+{
+    public static class StartUp
+    {
+        public static void Login()
+        {
+            //Show the Login View
+            AuthenticationViewModel viewModel = new AuthenticationViewModel(new AuthenticationService());
+            IView loginWindow = new LoginWindow(viewModel);
+            loginWindow.Show();
+        }
+        
+        public static void StartApp(AuthenticationViewModel vm)
+        {
+            if (vm.IsAuthenticated)
+            {
+                // Load Main Window
+                var mainWindow = new MainWindow();
+                var mvm = new MainWindowModel();
+                mainWindow.DataContext = mvm;
+                mainWindow.Show();
+            }
+        }
+
+    }
+}
