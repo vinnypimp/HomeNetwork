b4af9344d4e33215d32a8f3043465914a82e9dbe
vinnypimp <vpimpinella@gmail.com>
2016-04-11 13:05:36 -0400

    NewUser Changes

---
+++ HomeNetworkApp/HomeNetwork/NewUserRegistration/Model/NewUser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+
+namespace NewUserRegistration.Model
+{
+    public class NewUser : INotifyPropertyChanged
+    {
+        private string _userName;
+        private string _password;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public string UserName { get { return _userName; } set { _userName = value; OnPropertyChanged("UserName"); } }
+        public string Password { get { return _password; } set { _password = value; OnPropertyChanged("Password"); } }
+        public string FirstName { get { return _firstName; } set { _firstName = value; OnPropertyChanged("FirstName"); } }
+        public string LastName { get { return _lastName; } set { _lastName = value; OnPropertyChanged("LastName"); } }
+        public string Email { get { return _email; } set { _email= value; OnPropertyChanged("Email"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ HomeNetworkApp/HomeNetwork/NewUserRegistration/ViewModel/UserRegistrationViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Authentication;
+using NewUserRegistration.Model;
+using NewUserRegistration.View;
+using Authentication;
+
+namespace NewUserRegistration.ViewModel
+{
+    public class UserRegistrationViewModel : ViewModelBase
+    {
+        #region Fields
+        
+        // Property Variables
+        private NewUser _newUser;
+
+        #endregion
+
+        #region Constructors
+            
+        public UserRegistrationViewModel()
+        {
+            this.Initialize();
+        }
+
+        #endregion  
+
+        #region Command Properties
+
+        private bool CanRegister()
+        {
+            // process Registration Validation
+            return true;
+        }
+
+        private ICommand _process;
+        public ICommand Process
+        {
+            get
+            {
+                if(_process == null)
+                {
+                    _process = new RelayCommand(
+                        param => this._RegisterUser(),
+                        param => this.CanRegister());
+                }
+                return _process;
+            }
+
+        }
+
+        #endregion
+
+        #region Data Properties
+
+        public NewUser NewUser
+        {
+            get { return _newUser; }
+            
+            set
+            {
+                _newUser = value;
+                base.OnPropertyChanged("NewUser");
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+
+
+        #endregion
+
+        #region Private Methods
+
+        private void Initialize()
+        {
+            UserRegistration urv = new UserRegistration();
+            urv.DataContext = this;
+            urv.Margin = new Thickness(0, 0, 0, 0);
+
+            _newUser = new NewUser();
+        }
+
+        private void RegisterUser(NewUser user)
+        {
+            
+        }
+
+        #endregion  
+
+
+    }
+}
